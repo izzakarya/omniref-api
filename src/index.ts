cd48c9509bef1d42b9fc4cb59f8bf7317982cabb
Imam Satibin <imam.satibin@gmail.com>
2026-06-29 09:27:49 +0700

    Bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import { getCountries, getStatesOfCountry, getCitiesOfState } from '@countrystatecity/countries';
+import { getCountries, getStatesOfCountry, getCitiesOfState, ICountry } from '@countrystatecity/countries';
 import dataIcdDental from './data/icd-dental.json'
 
 const app = new Hono()
@@ -9,21 +9,22 @@ const app = new Hono()
 app.use('/api/*', cors())
 
 // Endpoint Utama
-app.get('/', (c) => c.text('Lexicon API is running! 🚀'))
+app.get('/', (c) => c.text('Service is running! 🚀'))
 
-// Endpoint Negara (Ubah jadi sync, hapus async/await)
-app.get("/api/countries", (c) => {
-  const rawCountries = getCountries();
+
+app.get("/api/countries", async (c) => {
+  const rawCountries = await getCountries(); 
   
   if (!rawCountries) {
     return c.json({ error: "Gagal mengambil data negara" }, 500);
   }
 
-  const countries = rawCountries.map((country) => ({
-    code: country.isoCode,
+  const countries = rawCountries.map((country: ICountry) => ({
+    code: country.iso2,
     name: country.name,
+    native: country.native,
     phonecode: country.phonecode,
-    flag: country.flag
+    flag: country.emoji
   }));
   
   return c.json(countries);
